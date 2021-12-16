514e1e07264a8776999f4c0dabbf08beb930bc7c
yair <aythb@hotmail.com>
2021-12-15 18:39:37 -0600

    se agrega el codigo base para los eventos del mouse

---
+++ Events.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+def dibujando (event,x,y,flag,param):
+    #aqui se imprimen la informacion sobre los eventos que se estan realizando
+    print('-----------------')
+    print('event', event)
+    print('x=',x)
+    print('y=',y)
+    print('flags',flag)
+    
